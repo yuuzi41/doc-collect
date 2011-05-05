70b43650ad1e60a82d85158db658850c6d443a0a
yuuzi <yuuzi@home.memorize-being.net>
2011-05-05 11:29:07 +0900

    fix to support win webfolder

--- app/controllers/fakedav_controller.rb
+++ app/controllers/fakedav_controller.rb
@@ -151,8 +151,8 @@ end
 	  when "infinity" then render(:nothing => true, :status => :forbidden) and return
 	  end
 
-	  if params['propfind'].nil? then render(:nothing => true, :status =>:ok) and return
-	  if params['propfind'].count > 1 then render(:nothing => true, :status =>:bad_request) and return
+	  render(:nothing => true, :status =>:ok) and return if params['propfind'].nil?
+	  render(:nothing => true, :status =>:bad_request) and return if params['propfind'].count > 1
 	  params['propfind'].each_pair do |k,v|
         case k
 	    when 'allprop' then render(:xml => self.generate_allprop(data), :status => :multi_status) and return
@@ -200,8 +200,8 @@ end
 	  when "infinity" then render(:text => '', :status => :forbidden) and return
 	  end
 
-	  if params['propfind'].nil? then render(:nothing => true, :status =>:ok) and return
-	  if params['propfind'].count > 1 then render(:nothing => true, :status =>:bad_request) and return
+  	  render(:nothing => true, :status =>:ok) and return if params['propfind'].nil?
+	  render(:nothing => true, :status =>:bad_request) and return if params['propfind'].count > 1
 	  params['propfind'].each_pair do |k,v|
         case k
 	    when 'allprop' then render(:xml => self.generate_allprop(data), :status => :multi_status) and return
@@ -388,8 +388,8 @@ end
 		when "infinity" then render(:nothing => true, :status => :forbidden) and return
 		end
 
-	    if params['propfind'].nil? then render(:nothing => true, :status =>:ok) and return
-	    if params['propfind'].count > 1 then render(:nothing => true, :status =>:bad_request) and return
+	    render(:nothing => true, :status =>:ok) and return if params['propfind'].nil?
+	    render(:nothing => true, :status =>:bad_request) and return if params['propfind'].count > 1
 		params['propfind'].each_pair do |k,v|
 		  case k
 		  when 'allprop' then render(:xml => self.generate_allprop(data), :status => :multi_status) and return
